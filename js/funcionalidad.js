6ceb8635ab729ea89411ae0716ea0bdfb46e7bf7
valerimon <vale246.vm@gmail.com>
2023-03-01 17:37:39 -0300

    subiendo mi proyecto

---
+++ js/funcionalidad.js
@@ -0,0 +1,14 @@
+document.getElementById('video').onclick = function () {
+    console.log("Evento exitoso")
+document.getElementById("fullstack").innerHTML= "¡Ahora conoces la importancia de contratarnos!";
+    
+}
+
+document.getElementById('button').onclick =function mostrar_alerta () {
+    alert("Solicitud de contacto enviada satisfactoriamente")
+}
+
+const formulario =document.getElementById('form')
+formulario.addEventListener('submit',  function(){
+    document.getElementById('empresa').value
+})
